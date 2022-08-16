81b59569a2458884d3c1b1fdfeee2e3b278135e8
arieldavis22 <amanichai100@gmail.com>
2022-08-16 16:47:03 -0400

    interface part 2

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -5,7 +5,12 @@ public class Main {
         ITelephone somePhone;
         somePhone = new DeskPhone(123456);
         somePhone.powerOn();
-        somePhone.callPhone(682374);
+        somePhone.callPhone(123456);
+        somePhone.answer();
+
+        somePhone = new MobilePhone(1238907);
+//        somePhone.powerOn();
+        somePhone.callPhone(1238907);
         somePhone.answer();
     }
 }

e480b0b05be8da98a3bb258ac103f37265c85d20
arieldavis22 <amanichai100@gmail.com>
2022-08-16 21:07:41 -0400

    inner class 2

--- src/InnerClasses/Main.java
+++ src/InnerClasses/Main.java
@@ -1,15 +1,50 @@
 package InnerClasses;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
     public static void main(String[] args) {
         // Static nested classes
         // NonStatic nested class (Inner class)
         // Local class (Inner class defined inside a scope block)
         // Anonymous class (Nested class without a class name)
 
-        GearBox mcLaren = new GearBox(6);
-        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        // Local Class
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener() {
+//                System.out.println("Ive been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//
+//        btnPrint.setOnClickListener(new ClickListener());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+    }
+    private static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
